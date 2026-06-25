6cb5c2ebc48dc35506a07bcc0681ac4cd1c12313
unknown <pas082bct004@wrc.edu.np>
2026-06-25 07:46:44 +0545

    second day

--- app.py
+++ app.py
@@ -2,9 +2,34 @@ from flask import Flask,render_template,request,redirect
 
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def home():
-    return render_template("index.html")
+    result = None
+    if request.method == "POST":
+        from_unit= request.form["from_unit"]
+        to_unit= request.form["to_unit"]
+
+        value = float(request.form["value"])
+        result = convert(
+            value,
+            from_unit,
+            to_unit
+        )
+
+    return render_template("index.html", test=result)
+
+
+def convert(value,from_unit,to_unit):
+
+    if from_unit == "meter" and to_unit == "kilometer":
+        return value/1000
+    elif from_unit == "kilometer" and to_unit == "meter":
+        return value * 1000
+
+    return value
+
+
+
 
 
 if __name__ == '__main__':
